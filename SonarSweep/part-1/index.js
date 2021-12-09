473bcbe06b2305312ec53ce1d5187e618e51cf94
G_Anta <ganta@telecomboutique.com>
2021-12-09 12:27:26 +0100

    rename folders

--- SonarSweep/part-1/index.js
+++
@@ -1,7 +0,0 @@
-
-const { input } = require("./input");
-
-let increaseCounter = 0;
-const countIncreases = (value, index, array) => { if(index > 0 && value > array[index - 1]) increaseCounter++ }
-input.forEach(countIncreases);
-console.log(increaseCounter);
